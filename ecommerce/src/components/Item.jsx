c423337231d21689bc3a0a9a692f2c2c3d68e45d
Omar D'agostino <omardagostino@gmail.com>
2023-07-11 13:57:16 -0300

    version 4

--- ecommerce/src/components/Item.jsx
+++ ecommerce/src/components/Item.jsx
@@ -1,23 +1,7 @@
 import Button from "react-bootstrap/Button"
-// import React, { useState, useEffect } from 'react';
 import Card from "react-bootstrap/Card"
 import { Link } from "react-router-dom"
 export const Item = ({moneda}) => {
-   // const [imagenSrc, setImagenSrc] = useState('');
-    
-  /* useEffect(() => {
-    /import(`../assets/imagenes/${moneda.imagen}`)
-      .then((module) => {
-        setImagenSrc(module.default);
-      })
-      .catch((error) => {
-        console.log('Error al importar la imagen:', error);
-        // Aquí puedes establecer una imagen de reemplazo en caso de error
-      });
-  }, [moneda.imagen]);*/
-    
-  console.log (moneda.imagen)
-  
   return (
         <Card
         style={{ width:"18rem"}}
@@ -36,6 +20,4 @@ export const Item = ({moneda}) => {
         </Card>
     ) 
     }
-    /* <Link to={`/item/${moneda.id}`}>
-                <Button variant="primary">ver detalle </Button>
-            </Link> */
+   
